94cabfa2e43dd0c86ac6b0b98b53a69b5bc793c0
milkov97 <kristiyanmilkov@abv.bg>
2023-07-18 18:45:01 +0300

    add pop method

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,30 +1,45 @@
 class Node {
-    constructor(value) {
-        this.prev = null;
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.prev = null;
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class DoublyLinkedList {
-    constructor(value) {
-        const newNode = new Node(value)
-        this.head = newNode;
-        this.tail = this.head;
-        this.length = 1;
-    }
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
 
-    push(value) {
-        const newNode = new Node(value)
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    const temp = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
